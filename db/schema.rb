34bf88ef5e0c183509f1061f7e24c18f855e2512
Taci Shlosberg <tshlosberg@gmail.com>
2020-08-18 13:27:15 -0700

    Changed the migration and added email username to users

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,9 @@ ActiveRecord::Schema.define(version: 2020_08_17_171503) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
