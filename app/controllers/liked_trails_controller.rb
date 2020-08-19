288bf80f4b2a0a451cdfdcc0bcf8992d506fbc7b
bdb2381 <brad.beggs@gmail.com>
2020-08-18 21:10:49 -0700

    rough code for liking a trail

--- app/controllers/liked_trails_controller.rb
+++ app/controllers/liked_trails_controller.rb
@@ -1,6 +1,25 @@
 class LikedTrailsController < ApplicationController
   before_action :authenticate_user!
+  
+  
+  def liked_trail
+    @liked_trail = current_user.liked_trails.new(trail_id: params[:article_id])
 
+    if @liked_trail.save
+      flash[:succcess] = "Saved this trail."a
+      redirect_to trail_path
+      
+    else 
+      flash[:error] = "Couldn't save the trail"
+      redirect_to trail_path
+    end
+    
+    
+  end 
+
+# To verify if a user is signed in, use the following helper:  user_signed_in?
+# For the current signed-in user, this helper is available: current_user
+# You can access the session for this scope: user_session
 
 
 
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,6 @@
 class Trail < ApplicationRecord
   belongs_to :park
   has_many :activities
+  has_many :liked_trails
+  has_many :users, through: :liked_trails
 end
