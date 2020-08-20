6bb69caecc5a49cde4cd84545ccd1bc931b713fe
bdb2381 <brad.beggs@gmail.com>
2020-08-20 15:00:45 -0700

    changed routes for /login and /join

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    get '/join', to: 'devise/registrations#new'
+  end
+
   devise_for :users
   resources :equipment
   resources :liked_trails
@@ -20,9 +25,6 @@ get "map", to: "parks#index"
 get "trails/state/:state", to: "trails#index", as: "trail_state"
 get "parks/state/:state", to: "parks#index", as: "park_state"
 
-devise_scope :user do
-  get 'login', to: 'devise/sessions#new'
-  get 'join', to: 'devise/registrations#new'
-end
+
 
 end
