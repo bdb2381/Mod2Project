b7dacddc148ce3407f6c06d0c7052aa48e7e1b58
bdb2381 <brad.beggs@gmail.com>
2020-08-19 21:20:30 -0700

    tweaks to devise route names, join and login styling to match logged in style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :users
   resources :equipment
   resources :liked_trails
   resources :users, only: [:show, :edit]
@@ -19,9 +19,17 @@ get "map", to: "parks#index"
 
 get "trails/state/:state", to: "trails#index", as: "trail_state"
 
-devise_scope :user do
-  get 'login', to: 'devise/sessions#new'
-  get 'join', to: 'devise/registrations#new'
-end
+devise_for :users,
+  :path => "usuarios",
+  :path_names => {
+    :sign_in => 'login',
+    :sign_out => 'logout',
+    :password => 'secret',
+    :confirmation => 'verification',
+    :unlock => 'unblock',
+    :registration => 'register',
+    :sign_up => 'join' }
 
-end
+
+
+end # end draw do 
