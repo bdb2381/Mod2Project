c771262df1889ca280ff73b858d6c33ee327f5b0
bdb2381 <brad.beggs@gmail.com>
2020-08-21 11:27:16 -0700

    fixed flash alert for liked trails

--- app/controllers/liked_trails_controller.rb
+++ app/controllers/liked_trails_controller.rb
@@ -2,9 +2,13 @@ class LikedTrailsController < ApplicationController
   before_action :authenticate_user!
   
   def create 
-    if LikedTrail.find_or_create_by(trail_id: params[:trail_id], user_id: current_user.id)
-      # flash[:notices]
-      redirect_to trail_path(params[:trail_id]), notice: "Trail Added to My Likes" 
+    # if LikedTrail.find_or_create_by!(trail_id: params[:trail_id], user_id: current_user.id)
+    if LikedTrail.find_by(trail_id: params[:trail_id], user_id: current_user.id)
+      redirect_to trail_path(params[:trail_id]), alert: "You Liked This Trail Already." 
+      
+    else
+      LikedTrail.create(trail_id: params[:trail_id], user_id: current_user.id)
+      redirect_to trail_path(params[:trail_id]), notice: "Trail Added To Your Liked Trails."
     end 
     
   end
