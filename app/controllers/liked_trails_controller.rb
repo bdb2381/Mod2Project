327f477bcca4936eefc4fa24a2755bdca6f87988
bdb2381 <brad.beggs@gmail.com>
2020-08-21 12:08:58 -0700

    minor text changes

--- app/controllers/liked_trails_controller.rb
+++ app/controllers/liked_trails_controller.rb
@@ -2,7 +2,6 @@ class LikedTrailsController < ApplicationController
   before_action :authenticate_user!
   
   def create 
-    # if LikedTrail.find_or_create_by!(trail_id: params[:trail_id], user_id: current_user.id)
     if LikedTrail.find_by(trail_id: params[:trail_id], user_id: current_user.id)
       redirect_to trail_path(params[:trail_id]), alert: "You Liked This Trail Already." 
       
@@ -12,6 +11,16 @@ class LikedTrailsController < ApplicationController
     end 
     
   end
+
+
+
+  private
+    def liked_params
+      params.require(:likedtrail).permit(:trail_id) # doesn't work. unlcear of syntax for :likedtrail
+    end
+
+
+
 
 # To verify if a user is signed in, use the following helper:  user_signed_in?
 # For the current signed-in user, this helper is available: current_user
--- config/routes.rb
+++ config/routes.rb
@@ -6,23 +6,20 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  resources :equipment
-  resources :liked_trails
   resources :users, only: [:show, :edit]
-  resources :activities
-  resources :parks
-  resources :trails
+  resources :liked_trails, only: [:create]
+  resources :activities, only: [:index]
+  resources :parks, only: [:index, :show]
+  resources :trails, only: [:index, :show]
 
 
 root to: "homepage#index"
-
-get "map", to: "parks#index"
 
 get "/liked_trails", to: "liked_trails#create"
 post "/liked_trails", to: "liked_trails#create"
 
 get "trails/state/:state", to: "trails#index", as: "trail_state"
-get "parks/state/:state", to: "parks#index", as: "park_state"
+
 
 
 
